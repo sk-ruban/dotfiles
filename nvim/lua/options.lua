550372de241a8db92a5ce117af0913b2ed069991
Ruban <65346010+sk-ruban@users.noreply.github.com>
2024-09-06 22:13:55 +0800

    Add neovim dotfiles

---
+++ nvim/lua/options.lua
@@ -0,0 +1,24 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.clipboard = "unnamedplus" -- Syncs the system clipboard to nvim
+vim.opt.scrolloff = 999
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+
+vim.opt.termguicolors = true -- Enables 24bit colour support
+
+vim.g.mapleader = " "
---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,98 @@
+-- Sets up lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    {
+        "sainnhe/gruvbox-material",
+        config = function()
+            vim.g.gruvbox_material_background = 'medium'
+            vim.g.gruvbox_material_palette = 'material'
+            vim.cmd.colorscheme("gruvbox-material")
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+
+                incremental_selection = {  -- Press a single key and TS will select part of the file
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<Leader>ss", 
+                        node_incremental = "<Leader>si",
+                        scope_incremental = "<Leader>sc",
+                        node_decremental = "<Leader>sd",
+                    },
+                },
+
+                textobjects = {
+                    select = {
+                        enable = true,
+
+                        lookahead = true,
+
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V', -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    {
+        "neovim/nvim-lspconfig",
+    },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup_handlers ({
+                function (server_name) 
+                    require("lspconfig")[server_name].setup({})
+                end,
+            })
+        end,
+    },
+    {
+        'arnamak/stay-centered.nvim',
+        config = function()
+            require('stay-centered').setup()
+        end,
+    },
+})
